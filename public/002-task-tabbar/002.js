e0775eb317c2e19228fd6b2d566bfd2445f897a3
lchenaddus <lchen@addus.com>
2024-12-10 01:05:07 -0600

    create a karaoke effect with js, css, html.

--- public/002-task-tabbar/002.js
+++
@@ -1,8 +0,0 @@
-const divs=document.querySelectorAll('.tabbar div');
-
-divs.forEach((item)=>{
-    item.addEventListener('mouseover',function(e){
-        let left=e.clientX - item.getBoundingClientRect().x;
-        item.style.setProperty('--left', left+'px');
-  })
-})
